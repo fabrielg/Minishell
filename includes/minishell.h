6e12773085828a542cd0ea67b389aceb55bb77c0
Fabrielg <gfrancois2003@gmail.com>
2025-08-16 17:30:32 +0200

    feat: add the minishell struct with all variables, except the ast_root

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,16 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gfrancoi <gfrancoi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fabrielg <fabrielg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 02:06:44 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/08/16 02:07:08 by gfrancoi         ###   ########.fr       */
+/*   Updated: 2025/08/16 17:28:35 by fabrielg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdbool.h>
+# include <sys/types.h>
 # include "tokens.h"
+
+/* Main structure containing all shell data */
+typedef struct s_minishell
+{
+	char		**envp;					/* Copy of environment (modifiable) */
+	char		**export_list;			/* Exported variables (KEY=VALUE format) */
+
+	// TODO: t_ast	*ast_root;			/* Root of AST */
+	t_token		*ast_root;				/* Root of the AST */
+	char		*input_line;			/* Raw input line from readline */
+	t_token		*tokens;				/* List of tokens after lexing */
+
+	/* Return codes and status */
+	int			last_exit_code;			/* Return code of last command ($?) */
+	int			shell_exit_code;		/* Exit code of the shell */
+	bool		should_exit;			/* Flag to terminate the shell */
+	
+	/* History and readline */
+	char		**history;				/* Custom history */
+	
+	/* Signal management */
+	int			signal_received;		/* Last signal received */
+	bool		in_child_process;		/* To adapt signal behavior */
+	
+	/* Processes and pipes */
+	pid_t		*child_pids;			/* PIDs of active child processes */
+	int			pipe_fds[2];			/* File descriptors for pipes (temporary) */
+	
+	/* Session variables */
+	char		*current_directory;		/* Current PWD */
+	int			stdin_backup;			/* Backup of FD for redirections */
+	int			stdout_backup;			/* Backup of FD for redirections */
+	
+	/* Debugging/Development */
+	bool		debug_mode;				/* To display debug info */
+	char		*shell_name;			/* Shell name for error messages */
+}	t_minishell;
 
 #endif
