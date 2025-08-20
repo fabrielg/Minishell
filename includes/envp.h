a07088d6c980c0d5893fff28043a1fd65ed2d2bb
Alexandre <alexandredeabreu711@gmail.com>
2025-08-20 18:39:03 +0200

    refactor : depth form print func removed

--- includes/envp.h
+++ includes/envp.h
@@ -58,7 +58,7 @@ t_dic	*split_env_var(char *env_var);
 
 /*[BST UTILS]______________________*/
 
-void	print_tree(t_mst *tree, int depth);
+void	print_tree(t_mst *tree);
 t_mst	*mst_get_parent(t_mst *tree, char *child);
 t_mst	*mst_get_node(t_mst *tree, char *to_find);
 int		mst_insertion(t_mst **tree, t_mst *node);
