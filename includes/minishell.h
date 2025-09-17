07a9230f07f45ed5ff3fd33f00e711464a0f7a63
Alexandre <alexandredeabreu711@gmail.com>
2025-09-17 23:35:42 +0200

    refactore : removed unused vars

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 02:06:44 by gfrancoi          #+#    #+#             */
-/*   Updated: 2025/09/15 01:09:40 by alde-abr         ###   ########.fr       */
+/*   Updated: 2025/09/17 23:35:02 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,19 @@
 /* Main structure containing all shell data */
 typedef struct s_minishell
 {
-	char		**envp;					/* Copy of environment (modifiable) */
 	t_mst		*exports;				/* Exported variables (KEY=VALUE format) */
 
 	// TODO: t_ast	*ast_root;			/* Root of AST */
-	t_token		*ast_root;				/* Root of the AST */
 	char		*input_line;			/* Raw input line from readline */
 	t_list2		*tokens;				/* List of tokens after parsing */
 
 	/* Return codes and status */
 	int			last_exit_code;			/* Return code of last command ($?) */
-	int			shell_exit_code;		/* Exit code of the shell */
-	bool		should_exit;			/* Flag to terminate the shell */
 
 	/* History and readline */
 	char		**history;				/* Custom history */
 	char		*shell_name;			/* The prompt display by readline */
-	
+
 	/* Signal management */
 	int			signal_received;		/* Last signal received */
 	bool		in_child_process;		/* To adapt signal behavior */
@@ -50,7 +46,6 @@ typedef struct s_minishell
 	int			pipe_fds[2];			/* File descriptors for pipes (temporary) */
 
 	/* Session variables */
-	char		*current_directory;		/* Current PWD */
 	int			stdin_backup;			/* Backup of FD for redirections */
 	int			stdout_backup;			/* Backup of FD for redirections */
 }	t_minishell;
