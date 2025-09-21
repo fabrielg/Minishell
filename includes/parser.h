c86a258b9d16a3f2523939d8a10188655ab0c9b1
Fabrielg <gfrancois2003@gmail.com>
2025-09-21 20:00:43 +0200

    fix: add the right parser protocol function

--- includes/parser.h
+++ includes/parser.h
@@ -17,8 +17,8 @@
 # include "minishell.h"
 
 /* Functions utils for parsing */
-t_list2	*parser(char *command_line);
-char	**glob_token(const char *token, int *out_count);
+t_list2	*parser(char *command_line, t_mst *env, int exit_code);
+void	glob_args_commands(t_list2 *tokens);
 void	expander(t_list2 *tokens, t_mst *env, int exit_code);
 t_list2	*group_commands(t_list2 *tokens);
 t_list2	*tokenize(char **contents);
