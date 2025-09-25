ff10244bc9dae9795b053a86b27c6e8734622624
Alexandre <alexandredeabreu711@gmail.com>
2025-09-25 02:09:10 +0200

    feat : added heredoc handle

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,7 +2,7 @@
 #include "lexer.h"
 #include "parser.h"
 
-t_list2	*parser(char *command_line, t_mst *env, int exit_code)
+t_list2	*parser(char *command_line, t_minishell *ms)
 {
 	char	**contents;
 	t_list2	*tokens;
@@ -19,7 +19,9 @@ t_list2	*parser(char *command_line, t_mst *env, int exit_code)
 	tokens = group_commands(tokens);
 	if (!tokens)
 		return (NULL);
-	expander(tokens, env, exit_code);
+	if (handle_heredocs(tokens, ms))
+		return (NULL);
+	expander(tokens, ms->exports, ms->last_exit_code);
 	if (!tokens)
 		return (NULL);
 	glob_commands(tokens);
