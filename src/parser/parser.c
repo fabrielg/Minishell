ba06cdd9db73d15cc8dffdb6124bed0eb829cc68
Fabrielg <gfrancois2003@gmail.com>
2025-09-25 02:35:31 +0200

    feat: trim input line

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,13 @@ t_list2	*parser(t_minishell *ms)
 {
 	char	**contents;
 	t_list2	*tokens;
+	char	*line_trim;
 
+	line_trim = ft_strtrim(ms->input_line, " \a\b\t\n\v\f\r");
+	if (!line_trim)
+		return (NULL);
+	free(ms->input_line);
+	ms->input_line = line_trim;
 	if (lex_line(ms->input_line) == 2)
 		return (NULL);
 	contents = smart_split(ms->input_line);
