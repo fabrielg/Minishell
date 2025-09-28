07c1d60632799511f2f82d0f0006ac2abf7e9d6b
Fabrielg <gfrancois2003@gmail.com>
2025-09-28 15:20:40 +0200

    refactor: show last exit code update if DEBUG_MODE is on

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -10,10 +10,10 @@ int	exec(t_command *cmd, t_minishell *ms)
 	if (cmd->args[0] != 0 && get_builtin(cmd->args[0], NULL))
 	{
 		ms->last_exit_code = execute_one_builtin(cmd, ms);
-		printf("last exit code : %i\n", ms->last_exit_code);
+		if (DEBUG_MODE)
+			printf("last exit code : %i\n", ms->last_exit_code);
 		return (0);
 	}
 	ms->last_exit_code = execute_cmd(cmd, ms);
-	// printf("last exit code : %i\n", ms->last_exit_code);
 	return (0);
 }
