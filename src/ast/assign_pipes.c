0662ecd8657eae13e471c62ed5b90e3eadf67506
Fabrielg <gfrancois2003@gmail.com>
2025-09-29 14:02:28 +0200

    fix: assign pipes for consecutive pipeline

--- src/ast/assign_pipes.c
+++ src/ast/assign_pipes.c
@@ -1,8 +1,24 @@
 #include "ast.h"
 
+static t_command	*find_rightmost_cmd(t_ast *node)
+{
+	if (!node)
+		return NULL;
+	if (node->type == TOKEN_COMMAND)
+		return node->cmd;
+	else if (node->type == TOKEN_PIPELINE)
+		return find_rightmost_cmd(node->pipeline.right);
+	else if (node->type == TOKEN_LOGICAL_EXPRESSION)
+		return find_rightmost_cmd(node->logical.right);
+	else if (node->type == TOKEN_SUBSHELL)
+		return find_rightmost_cmd(node->subshell);
+	return NULL;
+}
+
 int	assign_pipes(t_ast *node)
 {
-	int	fd[2];
+	int			fd[2];
+	t_command	*left_most;
 
 	if (!node)
 		return (0);
@@ -10,12 +26,14 @@ int	assign_pipes(t_ast *node)
 	{
 		if (pipe(fd) == -1)
 			return (perror("pipe"), 1);
-		if (node->pipeline.left && node->pipeline.left->type == TOKEN_COMMAND)
-			node->pipeline.left->cmd->pipes[1] = fd[1];
+		left_most = find_rightmost_cmd(node->pipeline.left);
+        if (left_most)
+            left_most->pipes[1] = fd[1];
 		if (node->pipeline.right && node->pipeline.right->type == TOKEN_COMMAND)
 			node->pipeline.right->cmd->pipes[0] = fd[0];
+		else
+			assign_pipes(node->pipeline.right);
 		assign_pipes(node->pipeline.left);
-		assign_pipes(node->pipeline.right);
 	}
 	else if (node->type == TOKEN_LOGICAL_EXPRESSION)
 	{
